41879c9e644f74cbe4e715c5ecdfb41c1768a5b1
Steve <steve.c.cardozo@gmail.com>
2018-11-06 17:33:26 -0500

    updated books section using php

--- works.php
+++ works.php
@@ -5,7 +5,7 @@
 
 <div class="col-md-10 page-content">
   <h2 class="text-center page-content-title">PUBLISHED WORKS</h2>
-  <p class="text-center">Books and articles by Neil Shapiro. The articles are available to read on this website and the books may be purchased directly from Neil Shapiro or from the International Match Safe Association (<a href="http://www.matchsafe.org">http://www.matchsafe.org</a>).</p>
+  <p class="text-center">Books and articles by Neil Shapiro. The articles are available to read on this website and the books may be purchased directly from Neil Shapiro or from the International Match Safe Association (<a target="_blank" href="http://www.matchsafe.org">http://www.matchsafe.org</a>).</p>
   <p class="text-center" style="margin-bottom:30px;"><small><strong>ALL RIGHTS FOR COMMERCIAL USE ARE RESERVED. PERMISSION FOR NON-COMMERCIAL USE MAY BE OBTAINED BY CONTACTING NEIL SHAPIRO</strong></small></p>
   <hr>
   <h2 class="text-center page-content-title">ARTICLES</h2>
@@ -29,37 +29,19 @@
   <h2 class="text-center page-content-title">BOOKS</h2>
   <div class="book-slider">
 
-    <div class="card text-center">
-      <img class="img-fluid mx-auto" src="img/book.gif" alt="Book 1" height="120">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build about the book and book content which is inside the book.</p>
-      </div>
-      <a href="#" class="btn btn-primary">Read More</a>
-    </div>
-
-    <div class="card text-center">
-      <img class="img-fluid mx-auto" src="img/book.gif" alt="Book 1" height="120">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build about the book and book content which is inside the book.</p>
-      </div>
-      <a href="#" class="btn btn-primary">Read More</a>
-    </div>
-
-    <div class="card text-center">
-      <img class="img-fluid mx-auto" src="img/book.gif" alt="Book 1" height="120">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build about the book and book content which is inside the book.</p>
-      </div>
-      <a href="#" class="btn btn-primary">Read More</a>
-    </div>
-
-    <div class="card text-center">
-      <img class="img-fluid mx-auto" src="img/book.gif" alt="Book 1" height="120">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build about the book and book content which is inside the book.</p>
-      </div>
-      <a href="#" class="btn btn-primary">Read More</a>
-    </div>
+    <?php
+      $books = json_decode(file_get_contents("books.json"));
+      for ($x = 0; $x < count($books); $x++) {
+        echo '<div class="card text-center slide">
+                <img class="img-fluid mx-auto" src="img/books/' . $books[$x]->img . '" alt="Book ' . $x . '">
+                  <div class="card-body">
+                    <h6 class="card-title">' . $books[$x]->title . '</h6>
+                    <p class="card-text">' . $books[$x]->desc . '</p>
+                  </div>
+                  <a target="_blank" href="books/' . $books[$x]->link . '" class="btn btn-primary">Read More</a>
+              </div>';
+      }
+    ?>
 
   </div>
 </div>
