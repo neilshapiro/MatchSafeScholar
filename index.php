2e1455256807dee61b2e2b02b363ebc80b3d054d
Steve <steve.c.cardozo@gmail.com>
2018-11-12 21:58:37 -0500

    added subtitle to Gallery page

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   require_once("page_start.php");
 ?>
 
-<div class="col-md-10 page-content">
-  <h2 class="text-center page-content-title">GALLERY</h2>
+<div class="col-md-10 page-content text-center">
+  <h2 class="page-content-title">GALLERY</h2>
+  <h6 class="page-content-title">Selected match safes and match holders from worldwide collections.</h6>
   <div class="row">
     <div class="col-md-12">
       <div id="gallery">
@@ -14,7 +15,7 @@
           foreach ($captions as $num => $caption) {
             echo "<div class=''>
                     <img class='img-fluid mx-auto' src='img/gallery/$num.png' alt='Slide $num' style='max-height:500px'>
-                    <div class='caption text-center'>
+                    <div class='caption'>
                       $caption
                     </div>
                   </div>";
