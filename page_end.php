d9f8cdaaaf8f581ecca0d6fbc531da607ade3d85
Steve <steve.c.cardozo@gmail.com>
2018-11-13 21:28:41 -0500

    combine js into one

--- page_end.php
+++ page_end.php
@@ -1,9 +1,7 @@
 </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="js/jquery-popper-bootstrap.js"></script>
 
 <?php if($page == "gallery"): ?>
   <script type="text/javascript" src="slick/slick.min.js"></script>
@@ -20,7 +18,7 @@
   });
   </script>
 <?php elseif($page == "contact"): ?>
-  <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+  <script src="js/feather.min.js"></script>
   <script>
     feather.replace();
   </script>
