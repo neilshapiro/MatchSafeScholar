8580cad4b1df8216ac379fb8278ddc3fedeb48b8
Steve <steve.c.cardozo@gmail.com>
2018-11-14 23:40:13 -0500

    added new appraisal icon on Services page

--- services.php
+++ services.php
@@ -9,19 +9,19 @@
     <b><a href="contact.php">Contact me</a> if I can help you. If I can't help I will refer you to someone who can.</b></p>
   <hr>
   <div class="row">
-    <div class="col-md-5 text-center">
-      <img class="img-fluid" src="img/icons/research2.png" style="max-height: 200px;">
+    <div class="col-md-3 text-center">
+      <img class="img-fluid" src="img/icons/research2.png" style="max-height: 100px;">
     </div>
-    <div class="col-md-7">
+    <div class="col-md-9">
       <h2>RESEARCH</h2>
       <p>I can and sometimes will do a careful investigation about your match safe, vesta, and match holder. This includes trying to ascertain the maker, materials, period of manufacture, country of origin, and authenticity.</p>
     </div>
   </div>
   <div class="row" style="margin-top:30px;">
-    <div class="col-md-5 text-center">
-      <img class="img-fluid" src="img/icons/appraisals.png" style="max-height: 200px;">
+    <div class="col-md-3 text-center">
+      <img class="img-fluid" src="img/icons/appraisals.png" style="max-height: 100px;">
     </div>
-    <div class="col-md-7">
+    <div class="col-md-9">
       <h2>APPRAISALS</h2>
       <p>I only appraise match safes, vestas and match holders. Nothing else. I will estimate the quality, nature, and sometimes the value, of your match safes, vestas, and match holders.</p>
     </div>
