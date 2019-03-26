ac96fd7e5a0fedb1a56dc836587ce7d2a587ea75
Steve <steve.c.cardozo@gmail.com>
2019-03-26 05:14:04 -0400

    revisions of the layout for the articles

--- works2.php
+++ works2.php
@@ -29,51 +29,33 @@
   <li><a target="_blank" href="#"></a></li>
   <li><a target="_blank" href="#"></a></li>
 -->
-  <h3>American Match Safes</h3>
-  <div class="articles-section text-left">
-    <h6 class="text-center">Gorham</h6>
-    <div class="row articles-row">
+  <h3 class="articles-header">American Match Safes</h3>
+  <div class="articles-section text-center">
+    <div class="row">
       <div class="col-md-6">
+        <h6>Gorham</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/A BRIEF HISTORY OF GORHAM MATCH SAFES.pdf">A Brief History of Gorham Match Safes</a></li>
           <li><a target="_blank" href="articles/Gorham Martele (1) - proof.pdf">Gorham's Martele and Paris Exposition Match Safes</a></li>
           <li><a target="_blank" href="articles/THE GORHAM GREAT BRITAIN CONNECTION.pdf">The Gorham Great Britain Connection</a></li>
           <li><a target="_blank" href="articles/GORHAM signed MS and Antoine Heller.pdf">Gorham's Signed Match Safes and Antoine Heller</a></li>
-        <ul>
-      </div>
-      <div class="col-md-6">
-        <ul class="articles">
           <li><a target="_blank" href="articles/Mysterious_Gorham_Match_Safes.pdf">Mysterious Gorham Match Safes</a></li>
           <li><a target="_blank" href="articles/THE CASE OF THE CURIOUS GORHAM MARKS.pdf">The Case of the Curious Gorham Marks</a></li>
           <li><a target="_blank" href="articles/Gorham Match safes and their archival records_combo.pdf">Gorham Match Safes and their Archival Records</a></li>
           <li><a target="_blank" href="articles/GORHAMs unique inlay patents.pdf">Gorham's Unique Inlay Patents</a></li>
         </ul>
       </div>
-    </div>
-    <h6 class="text-center">Tiffany</h6>
-    <div class="row articles-row">
       <div class="col-md-6">
+        <h6>Tiffany</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/A Tiffany Match  Safe Story.pdf">A Tiffany Match Safe Story</a></li>
           <li><a target="_blank" href="articles/Identifying_Unmarked_Tiffany_cond.pdf">Identifying Unmarked Tiffany Match Safes</a></li>
-        </ul>
-      </div>
-      <div class="col-md-6">
-        <ul class="articles">
           <li><a target="_blank" href="articles/Tiffany safe essay 9.pdf">Tiffany Safe Essay</a></li>
         </ul>
-      </div>
-    </div>
-    <h6 class="text-center">Other</h6>
-    <div class="row articles-row">
-      <div class="col-md-6">
+        <h6>Other</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/A_Mixed_Metal_Whiting_cond.pdf">A Mixed Metal Whiting Safe in the Japanese Taste</a></li>
           <li><a target="_blank" href="articles/An_Assyrian_Revival_Match_Safe.pdf">An Assyrian Revival Match Safe</a></li>
-        </ul>
-      </div>
-      <div class="col-md-6">
-        <ul class="articles">
           <li><a target="_blank" href="articles/American_Match_Safe_Masterpieces_cond.pdf">American Match Safe Masterpieces</a></li>
           <li><a target="_blank" href="articles/1_The Japanese Match Safe That Wasn.pdf">The Japanese Match Safe That Wasn't</a></li>
         </ul>
@@ -83,28 +65,28 @@
 
 
   <h3 class="articles-header">European Match Safes (Vestas)</h3>
-  <div class="articles-section text-left">
+  <div class="articles-section text-center">
     <div class="row">
       <div class="col-md-6">
-        <h6 class="text-center">United Kingdom</h6>
+        <h6>United Kingdom</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/Mr.Jorrocks vesta cases.pdf">Mr. Jorrock's Vesta Cases</a></li>
           <li><a target="_blank" href="articles/Mikado and other theatrical match safes.pdf">The Mikado and Other Theatrical Match Safes</a></li>
           <li><a target="_blank" href="articles/Cost of One Matchsafe.pdf">The Cost of One Match Safe in 1903 and in 2000</a></li>
         </ul>
-        <h6 class="text-center">France</h6>
+        <h6 style="margin-top:20px;">Russia</h6>
+        <ul class="articles">
+          <li><a target="_blank" href="articles/RUSSIAN MATCH HOLDERS.pdf">Russian Match Holders</a></li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h6>France</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/Cartier Essay.pdf">A Cartier Match Safe</a></li>
           <li><a target="_blank" href="articles/My Roty essay.pdf">The Greatest French Engraver Made a Match Safe</a></li>
           <li><a target="_blank" href="articles/Lacademie des beaux arts.pdf">L'Academie des Beaux-Arts, Roty, and a Match Safe</a></li>
         </ul>
-      </div>
-      <div class="col-md-6">
-        <h6 class="text-center">Russia</h6>
-        <ul class="articles">
-          <li><a target="_blank" href="articles/RUSSIAN MATCH HOLDERS.pdf">Russian Match Holders</a></li>
-        </ul>
-        <h6 class="text-center">Spain</h6>
+        <h6 style="margin-top:20px;">Spain</h6>
         <ul class="articles">
           <li><a target="_blank" href="articles/Damascene Match Safes1_9.13.18.pdf">Spanish and Japanese Damascene Match Safes</a></li>
         </ul>
@@ -113,18 +95,18 @@
   </div>
 
 
-  <h3 class="text-left articles-header">Japanese Match Safes</h3>
-  <div class="articles-section text-left">
+  <h3 class="articles-header">Japanese Match Safes</h3>
+  <div class="articles-section">
     <div class="row">
       <div class="col-md-6">
-        <ul class="articles">
+        <ul class="articles no-padding">
           <li><a target="_blank" href="articles/Sword match safes_good_better.pdf">Japanese Sword Safes: Good, Better, Best</a></li>
           <li><a target="_blank" href="articles/A MOKUME Gane Match safe.pdf">A Mokume Gane Match Safe</a></li>
           <li><a target="_blank" href="articles/Japanese patinas.pdf">Japanese Patinas</a></li>
         </ul>
       </div>
       <div class="col-md-6">
-        <ul class="articles">
+        <ul class="articles no-padding">
           <li><a target="_blank" href="articles/Japanese Silver Match Safes proof.pdf">Japanese Silver Match Safes</a></li>
           <li><a target="_blank" href="articles/Tips for the collector of Japanese Match Safes.pdf">Tips for the Collector of Japanese Match Safes</a></li>
         </ul>
@@ -133,29 +115,29 @@
   </div>
 
 
-  <h3 class="text-left articles-header">Global</h3>
-  <div class="articles-section text-left">
+  <h3 class="articles-header">Global</h3>
+  <div class="articles-section">
     <div class="row">
       <div class="col-md-12">
-        <ul class="articles">
+        <ul class="articles no-padding">
           <li><a target="_blank" href="articles/Match Safes around the World.pdf">Match Safes around the World</a></li>
         </ul>
       </div>
     </div>
   </div>
 
-  <h3 class="text-left articles-header">Match Holders</h3>
-  <div class="articles-section text-left">
+  <h3 class="articles-header">Match Holders</h3>
+  <div class="articles-section">
     <div class="row articles-row">
       <div class="col-md-6">
-        <ul class="articles">
+        <ul class="articles no-padding">
           <li><a target="_blank" href="articles/Animaliers_1_ essay.pdf">Animaliers and Match Holders</a></li>
           <li><a target="_blank" href="articles/Essay on Go-To_Bed.pdf">"GO-TO-BED" OR SIMPLY MATCH HOLDER?</a></li>
           <li><a target="_blank" href="articles/Friar match holder essay.pdf">A Clue to the Maker or Origin of the Friar/Monk Match Holder</a></li>
         </ul>
       </div>
       <div class="col-md-6">
-        <ul class="articles">
+        <ul class="articles no-padding">
           <li><a target="_blank" href="articles/Joseph Glanz_text_essay.pdf">Joseph Glanz’s Match Holders</a></li>
           <li><a target="_blank" href="articles/Essay_2_Zimmermanns9.15.17.pdf">A Tale of Two Match Holders</a></li>
           <li><a target="_blank" href="articles/Reconsidering a Match Holder.pdf">Reconsidering a Match Holder</a></li>
